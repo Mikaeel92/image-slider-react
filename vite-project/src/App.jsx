8db234a630c73c17c9fc29766ec636ec3695d83c
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-30 15:24:12 +0330

    state management

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [images, setImages] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const fetchData = async () => {
+
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className='text-3xl text-red-600'>App</div>
   )
